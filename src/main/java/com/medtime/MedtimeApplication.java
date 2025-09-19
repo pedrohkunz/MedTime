15d70489f5b1b77dd94533878493d98778c90d62
Pedro Kunz <pedrokunz094@gmail.com>
2025-09-19 00:11:28 -0300

    initial commit

---
+++ src/main/java/com/medtime/MedtimeApplication.java
@@ -0,0 +1,13 @@
+package com.medtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedtimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedtimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medtime/MedtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedtimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
